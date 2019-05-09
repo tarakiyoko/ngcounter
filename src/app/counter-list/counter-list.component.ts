584d245c8417f052ac2317f1a1546cee35120693
galvanize3 <galvanize3@fvfx1hk9j1wl.ci.org>
2019-05-09 09:47:36 -0600

    Super Counter Level 2

--- src/app/counter-list/counter-list.component.ts
+++ src/app/counter-list/counter-list.component.ts
@@ -8,25 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class CounterListComponent implements OnInit {
 
   counters: object[];
-  superCounters: [];
+  superCounters: object[];
   constructor() {
     this.counters = [];
+    this.superCounters = [];
   }
 
   ngOnInit() {
   }
 
   createCounter() {
+    // Check little counters for increments of 6
     if (this.counters.length !== 0 && this.counters.length % 6 == 0) {
-      const agg = this.counters.reduce((acc,val) => {
-        console.log(val);
-        return acc += val['detailCounter'];
-      },0);
-      console.log(agg);
-      // TODO: will need to create super counter.
-      // TODO: aggregate counter values in counters[]
-      this.counters = [];      
+
+      // Only want level 1 supers
+      const level1Super = this.superCounters.filter((val) => val['level'] === 1)
+
+      // Check level 1 supers for increments of 3
+      if (level1Super.length !== 0 && level1Super.length % 3 == 0) {
+        const aggL1Super = level1Super.reduce((acc,val) => {
+          return acc += val['super'];
+        },0);
+        // Aggregate little counters into super val
+        const agg = this.counters.reduce((acc,val) => {
+          return acc += val['detailCounter'];
+        },0);
+        let superCounter = {
+          super: aggL1Super + agg,
+          level: 2
+        }
+        this.superCounters.push(superCounter);
+        this.superCounters = this.superCounters.filter((val) => val['level'] !== 1);
+        this.counters = [];
+      } else {
+        // Aggregate little counters into super val and remove little counters
+        const agg = this.counters.reduce((acc,val) => {
+          return acc += val['detailCounter'];
+        },0);
+        let superCounter = {
+          super: agg,
+          level: 1
+        }
+        this.superCounters.push(superCounter);
+        this.counters = []; 
+      }   
     } else {
+      // little counter instantiate
       let obj = {
         detailCounter: 0
       }
--- src/app/super-counter/super-counter.component.ts
+++ src/app/super-counter/super-counter.component.ts
@@ -6,20 +6,23 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./super-counter.component.css']
 })
 export class SuperCounterComponent implements OnInit {
-  @Input() super:number;
+  @Input() super:object;
 
   constructor() { 
-    this.super = 0;
+    this.super = {};
   }
 
   ngOnInit() {
   }
 
   increment(num:number) {
-    this.super += num;
-    if(this.super <0) {
-      this.super=0;
+    this.super['super'] += num;
+    if(this.super['super'] <0) {
+      this.super['super']=0;
     }
   }
 
+  startIncrement() {
+    setInterval(() => this.super['super']++, 1000);
+  }
 }
